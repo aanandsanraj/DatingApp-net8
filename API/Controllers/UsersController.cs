bd6463d67d98a228d0646a8d21c7f1c9537531ac
Anandraj <aanandraj15@gmail.com>
2025-05-09 20:42:34 +0530

    Authentication and authorization added

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,16 +1,14 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController(ILogger<UsersController> logger, DataContext context) : Controller
+    public class UsersController(DataContext context) : BaseApiController
     {
-        private readonly ILogger<UsersController> _logger = logger;
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -19,6 +17,7 @@ namespace API.Controllers
             return users;
         }
 
+        [Authorize]
         [HttpGet("{id:int}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
@@ -30,12 +29,6 @@ namespace API.Controllers
             }
 
             return user;
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
         }
     }
 }
