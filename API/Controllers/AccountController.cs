0f4af40fc0bf01af7613126cabd35195489955ea
Anandraj <aanandraj15@gmail.com>
2025-05-12 16:21:15 +0530

    register and login completed

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {    
